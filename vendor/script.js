2d7067ce11876d302710ba91c805862a4e7d7056
Myo Min Thu <myominthu819@gmail.com>
2023-11-25 12:08:32 +0630

    updated

--- vendor/script.js
+++ vendor/script.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
     body.classList.toggle("show-sidebar");
   });
 });
+
+function toggleForm() {
+  const formContainer = document.querySelector(".form-container");
+  formContainer.classList.toggle("active");
+}
+
+function closeForm() {
+  const formContainer = document.querySelector(".form-container");
+  formContainer.classList.remove("active");
+}
+
+
+
+
+
+
+
+
